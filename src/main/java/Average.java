4e9576b538cee7d4159ef95f066babcf6c9169c4
rkcoden <khanra4@farmingdale.edu>
2023-02-08 23:57:19 +0000

    done

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,9 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for(int num : nums)
+            sum += num;
+        return (double) sum / nums.length;
     }
 }
